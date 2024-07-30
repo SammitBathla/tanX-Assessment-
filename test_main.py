751e4418e20a3316f6bd735943ec694d1d2c5ff7
unknown <sammitbatla9@gmail.com>
2024-07-30 19:57:13 +0530

    first commit

--- test_main.py
+++ test_main.py
@@ -1,20 +1,20 @@
 import unittest
 import pandas as pd
-from main import dataFrame, monthly_revenue, product_revenue, customer_revenue, top_10_Customers
+from main import dataFrame, monthlyRevenue, productRevenue, customerRevenue, top_10_Customers
 
 class TestOrdersData(unittest.TestCase):
 
     def test_monthlyRevenue(self):
-        self.assertGreater(len(monthly_revenue), 0)
+        self.assertGreater(len(monthlyRevenue), 0)
 
     def test_productRevenue(self):
-        self.assertGreater(len(product_revenue), 0)
+        self.assertGreater(len(productRevenue), 0)
 
     def test_customerRevenue(self):
-        self.assertGreater(len(customer_revenue), 0)
+        self.assertGreater(len(customerRevenue), 0)
 
     def test_top_10Customers(self):
-        self.assertEqual(len(top_10_Customers), 5)
+        self.assertEqual(len(top_10_Customers), 10)
 
 if __name__ == '__main__':
     unittest.main()
